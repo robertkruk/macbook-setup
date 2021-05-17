5c43f0ee9b16cd43872c9e0ed282ea644918dd89
Robert Kruk <18179387+robertkruk@users.noreply.github.com>
2021-05-17 13:40:23 +1000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,31 +8,28 @@ export PATH=/opt/homebrew/bin:$PATH
 brew update
 
 echo Installing basic homebrew softwares...
-brew install git php ruby node npm protobuf redis mysql rabbitmq mercurial wget zsh htop go mackup
+brew install git zsh
 
 # apps
 echo Installing desktop apps...
-brew install --cask google-chrome iterm2 folx dropbox adobe-acrobat-reader ledger-live textmate skype slack firefox vlc zoom viber microsoft-edge openemu anydesk microsoft-office steam
+brew install --cask adobe-acrobat-reader zoom microsoft-edge microsoft-office
 # utils
 echo Installing utilities...
-brew install --cask little-snitch istat-menus clipy grandperspective ipartition spectacle vipriser cd-to blackhole-2ch osxfuse handbrake the-unarchiver textbar adguard android-file-transfer balenaetcher sidequest openkey
+brew install --cask istat-menus clipy openvpn-connect the-unarchiver adguard
 # dev tools
 echo Installing dev tools...
-brew install --cask oracle-jdk docker sequel-ace visual-studio-code goland phpstorm tunnelblick dosbox-x postman wireshark altserver ifunbox sourcetree proxyman android-platform-tools zsh-syntax-highlighting
+brew install --cask visual-studio-code zsh-syntax-highlighting
 
 # default-writes
 echo Setting default-writes...
-sudo /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/ptrinh/macbook-setup/main/default-writes.sh)"
-
-cd Downloads
-curl -O https://raw.githubusercontent.com/MartinSeeler/iterm2-material-design/master/material-design-colors.itermcolors
+sudo /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/robertkruk/macbook-setup/main/default-writes.sh)"
 
 # oh-my-zsh
 echo Installing Oh-my-zsh...
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 
-echo "Next step: Sync Dropbox then use 'mackup restore' to restore dot files and other configurations"
+echo "Done"
 
 # To upgrade all softwares installed by cask later, just run this below command:
 # brew upgrade --cask --greedy
